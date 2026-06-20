ed9afd9448c2d840675c7caf9e477c32898970f3
SamFonDom <a23samfondom@inspedralbes.cat>
2026-06-20 19:23:33 +0200

    Movimiento de camara 3D

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,19 +5,60 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
     [SerializeField] float upForce = 5f;
+    [SerializeField] Transform cameraPivot;
+    [SerializeField] float sensitivity = 100f;
 
     private Rigidbody rb;
     private Vector2 moveInput;
+    private Vector2 lookInput;
+
+    private float xRotation = 0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        rb.freezeRotation = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y);
-        rb.AddForce(movement * speed);
+        //Movimiento relativo al player (no mundo)
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+
+        Vector3 movement = forward * moveInput.y + right * moveInput.x;
+        movement = movement.normalized;
+
+        rb.AddForce(movement * speed, ForceMode.Force);
+    }
+
+    private void LateUpdate()
+    {
+        Rotating();
+    }
+
+    void Rotating()
+    {
+        float mouseX = lookInput.x * sensitivity * Time.deltaTime;
+        float mouseY = lookInput.y * sensitivity * Time.deltaTime;
+
+        //Vertical (cámara)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -30f, 60f);
+
+        cameraPivot.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        //Horizontal (player) usando física
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, mouseX, 0f));
+    }
+
+    public void Looking(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
     }
 
     public void Moving(InputAction.CallbackContext context)
